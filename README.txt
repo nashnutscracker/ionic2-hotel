9872ac224983989bb775a288b42bdb39ff7f86b9
Ratnaparkhi <suyash.ratnaparkhi@accenture.com>
2018-08-14 12:46:05 +0530

    first commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+Read this article before running the app.
+
+https://www.joshmorony.com/building-a-hotel-booking-app-with-ionic-2-mongodb-node/
---
+++ ionic2-hotel/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { HttpModule } from '@angular/http';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { RoomsProvider } from '../providers/rooms/rooms';
+import { SearchPage } from '../pages/search/search';
+import { BookingPage } from '../pages/booking/booking';
+import { AvailableRoomsPage } from '../pages/available-rooms/available-rooms';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    AvailableRoomsPage,
+    BookingPage,
+    SearchPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    AvailableRoomsPage,
+    BookingPage,
+    SearchPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    RoomsProvider
+  ]
+})
+export class AppModule {}
